ccf36bca44d60afd469992ebdd50aa0bb100d36e
Joan Codina <joancf@users.noreply.github.com>
2018-04-11 11:33:50 +0200

    LIcense Updated

--- src/main/java/edu/upf/taln/uima/babelnet/BabelNetXMIReaderWriter.java
+++ src/main/java/edu/upf/taln/uima/babelnet/BabelNetXMIReaderWriter.java
@@ -1,3 +1,18 @@
+/*
+OpenMinted_Babelnet
+Copyright (C) 2018  grup TALN - Universitat Pompeu Fabra
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
 package edu.upf.taln.uima.babelnet;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
@@ -54,7 +69,7 @@ public class BabelNetXMIReaderWriter
 
 
         
-        // uset that language for the pipeline...
+        // use that language for the pipeline...
         
         SimplePipeline.runPipeline(
                 createReaderDescription(XmiReader.class,
