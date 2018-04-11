71ab4a1038303fb3bc7b71ccbf9d3d8d5c04b259
Joan Codina <joancf@users.noreply.github.com>
2018-04-11 11:18:29 +0200

    license info

--- src/main/java/edu/upf/taln/uima/babelnet/BabelnetSenseInventoryResource.java
+++ src/main/java/edu/upf/taln/uima/babelnet/BabelnetSenseInventoryResource.java
@@ -1,20 +1,17 @@
-/*******************************************************************************
- * Copyright 2016
- * Ubiquitous Knowledge Processing (UKP) Lab
- * Technische Universität Darmstadt
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
+/*
+OpenMinted_Babelnet
+Copyright (C) 2018  grup TALN - Universitat Pompeu Fabra
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/ 
 
 /**
  *
@@ -37,7 +34,7 @@ import it.uniroma1.lcl.jlt.util.Language;
 /**
  * A resource wrapping {@link BabelnetSenseInventory}
  *
- * @author <a href="mailto:miller@ukp.informatik.tu-darmstadt.de">Tristan Miller</a>
+ * @author <a href="mailto:joan.codina@upf.edu">Joan Codina</a>
  *
  */
 public class BabelnetSenseInventoryResource
@@ -72,6 +69,7 @@ public class BabelnetSenseInventoryResource
     public BabelnetSenseInventory getInvenory() {
         return (BabelnetSenseInventory) inventory;
     }
+    
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     public boolean initialize(ResourceSpecifier aSpecifier,
